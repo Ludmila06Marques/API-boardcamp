785a3e48710cbc86432b2b8214839b920ec5127e
Lud <instimila@gmail.com>
2022-07-25 12:34:41 +0200

    getCategories

---
+++ src/controllers/categoryController.js
@@ -0,0 +1,15 @@
+import db from '../postgressStrategy/db.js'
+
+export  async function  getCategories(req,res){
+
+    try {
+        const result = await db.query(' SELECT * FROM categories ')
+        res.send(result.rows)
+
+        
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+
+}
